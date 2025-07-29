81aa4f7dd1681c8ad8efd81a701d2433490c3f67
ImJarrs <sidikfs.fajar@gmail.com>
2025-07-29 13:25:02 +0700

    update error fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,25 +7,16 @@ use Illuminate\Http\Request;
 
 use App\Models\Banner;
 use App\Models\Card;
-use App\Models\Wristband;
+
 class ProductController extends Controller
 {
-    // RFID CARD
-        public function card(){
+    public function card(){
         $cards = Card::orderBy('created_at', 'desc')->get();
-        $cardBanner = Banner::orderBy('created_at', 'asc')->limit(1)->pluck('card_banner');
+        $cardBanner = Banner::orderBy('created_at', 'asc')->value('card_banner');
 
         return response()->json([
             'cards' => $cards,
             'cardBanner' => $cardBanner
         ]);
-    }
-
-    // RFID WRISTBAND
-        public function wristband(){
-        $wristbands = Wristband::orderBy('created_at', 'desc')->get();
-        $wristbandBanner = Banner::orderBy('created_at', 'asc')->limit(1)->pluck('wristband_banner');
-
-        return view('products.rfid-wristband', compact('wristbands', 'wristbandBanner'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Card;
-use App\Http\Resources\CardResource;
+
+use App\Http\Controllers\Api\ProductController;
 
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/card', function () {
-    return CardResource::collection(Card::all());
-});
+// Route::get('/card', function () {
+//     return CardResource::collection(Card::all());
+// });
+
+Route::get('/cards', [ProductController::class, 'card']);
